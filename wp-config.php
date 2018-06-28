dfdb5fb59a09765c03faba70da3736c366582234
TRAN DONG DUY TAN <trandongduytan@gmail.com>
2018-06-28 11:25:24 +0700

    Update

--- wp-config.php
+++ wp-config.php
@@ -17,10 +17,10 @@
  *
  * @package WordPress
  */
-
+define('FS_METHOD','direct');
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'taiphat');
+define('DB_NAME', 'vesinhdaiphat');
 
 /** MySQL database username */
 define('DB_USER', 'root');
